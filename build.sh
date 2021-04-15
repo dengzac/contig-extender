b7f20b35ba3c7cebb25c94ef40f51c25c27045c8
Zachary Deng <dengzac@gmail.com>
2021-04-14 23:52:17 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
-CFLAGS=-O0 python setup.py build_ext --inplace
+CFLAGS=-O0 python3 setup.py build_ext --inplace
 #cp build/lib.linux-x86_64-3.6/extender.cpython-36m-x86_64-linux-gnu.so .
-pyinstaller extender_wrapper.spec
+mv extender.so extender/extender.so
+python3 -m PyInstaller extender_wrapper.spec
 
-#mv extender.cpython-36m-x86_64-linux-gnu.so extender/extender.cpython-36m-x86_64-linux-gnu.so
 echo "Build successful: output dist/extender_wrapper"
--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ Options.embed = 'main'
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy as np
-
-
+import sysconfig, os
+class NoSuffixBuilder(build_ext):
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        suffix = sysconfig.get_config_var('EXT_SUFFIX')
+        ext = os.path.splitext(filename)[1]
+        return filename.replace(suffix, "") + ext
 # flags = subprocess.run(['python3-config', '--cflags', '--ldflags'], stdout=subprocess.PIPE).stdout.decode()
 # flags = flags.strip().replace('\n', ' -I' + np.get_include() + ' extender/extender.c ') + ' -fPIC -o extender/extend'
 # print(flags)
@@ -18,7 +23,7 @@ setup(
     version="0.0.1",
     ext_modules = [Extension("extender", ["extender/extender.pyx"])],
     include_dirs = [np.get_include()],
-    cmdclass={'build_ext': build_ext},
+    cmdclass={'build_ext': NoSuffixBuilder},
     scripts=['extender/extender_wrapper.py'],
     install_requires = [
         'numpy',
